0c41ad0808f51f98aa0d1a1887198afe1c622ce6
Arun Pillai <arun.vijayan.pillai@shortcut.no>
2021-01-17 23:46:25 +0100

    live for class

--- src/kristiania/kotlin/E-Collections-Loops.kts
+++ src/kristiania/kotlin/E-Collections-Loops.kts
@@ -1,24 +1,10 @@
 // While and For loops iwhi
-var i = 0
-while (i < 10) {
-    println(i)
-    i++
-}
 
-for (i in 0 until 10){
-    println(i)
-}
+
 
 // List in Immutable so to change items use MutableList
-val list : MutableList<Int> = mutableListOf<Int>(1,2,3,4,5,6)
-list.add(7)
-list.remove(2)
-// Filter the list, transform and print each item
-list.forEach { value ->
-    println(value)
-}
 
-list.filter { it<5 }.map { it *3 }.forEach { println(it) }
+// Filter the list, transform and print each item
 
 // Do this Exercise Write an extension function for String class to return the count count of vowels in that string.
 
